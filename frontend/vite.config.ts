32920fa36a14e8dba30b1202e8108d3fcc8adb77
Peter Shi <peterzzshi@gmail.com>
2025-12-29 01:09:33 +1100

    Docker build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/gmgn-clone/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
